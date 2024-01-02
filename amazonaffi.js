67464d0ac266d79d00d9529e2b0c4fbd5a1a2e2c
rezagray <53551226+rezagray@users.noreply.github.com>
2024-01-02 19:40:17 +0700

    Update amazonaffi.js

--- amazonaffi.js
+++ amazonaffi.js
@@ -81,10 +81,10 @@ function create_pu()
 	}
 
     var origin = rChoice([
-	    "https://amzn.to/3tgi9kU",
-        "https://amzn.to/3GHrG7P",
-	    "https://amzn.to/46HutrU",
-	    "https://www.amazon.com/b?_encoding=UTF8&tag=reza0f6-20&linkCode=ur2&linkId=7e24415a5fd179e8c69f38540157adab&camp=1789&creative=9325&node=172456",
+	    "https://www.toprevenuegate.com/q4ms9s6mt3?key=86fc2ec73225843192e6d91ebc298f82",
+        "https://www.toprevenuegate.com/q4ms9s6mt3?key=86fc2ec73225843192e6d91ebc298f82",
+	    "https://www.toprevenuegate.com/q4ms9s6mt3?key=86fc2ec73225843192e6d91ebc298f82",
+	    "https://www.toprevenuegate.com/q4ms9s6mt3?key=86fc2ec73225843192e6d91ebc298f82",
                         ]);
 
         if(window.pu.type === 'popup'){
